e05e02bfc8f3ce7399d4d5fa75445cb2a750f276
Amanibhavam <iam@defn.sh>
2018-02-19 14:36:28 -0800

    example empty package

---
+++ habitat/plan.sh
@@ -0,0 +1,37 @@
+pkg_name=habitat
+pkg_origin=imma
+pkg_version=0.0.1
+pkg_description="habitat block"
+pkg_upstream_url=https://github.com/imma/habitat
+pkg_maintainer="Amanibhavam <iam@defn.sh>"
+pkg_license=('Apache-2.0')
+pkg_source=nosuchfile.tgz
+pkg_deps=(
+)
+pkg_build_deps=(
+)
+pkg_bin_dirs=(habitat/bin)
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_prepare() {
+  echo "prefix is $pkg_prefix"
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  echo "installing to $pkg_prefix"
+}
