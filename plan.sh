ece018b48bb276173b40a5215cc762e104b030a5
Amanibhavam <iam@defn.sh>
2018-02-23 17:43:04 -0800

    minimum functions

--- plan.sh
+++ plan.sh
@@ -1,28 +1,11 @@
 pkg_name=habitat
 pkg_origin=imma
-pkg_version=0.0.1
-pkg_description="habitat block"
-pkg_upstream_url=https://github.com/imma/habitat
+pkg_version="0.1.0"
 pkg_maintainer="Amanibhavam <iam@defn.sh>"
-pkg_license=('Apache-2.0')
-pkg_source=nosuchfile.tgz
+pkg_license=("Apache-2.0")
 pkg_deps=(
   core/node
 )
-pkg_build_deps=(
-)
-
-do_download() {
-  return 0
-}
-
-do_verify() {
-  return 0
-}
-
-do_unpack() {
-  return 0
-}
 
 do_prepare() {
   return 0
